404f2318b4b9102e9dd78057a42f2f42c323f7f2
Robert Bedard <rbedard@plex.com>
2019-08-29 13:09:00 -0400

    slight improvement.

--- Readme.md
+++ Readme.md
@@ -3,6 +3,7 @@
 This is a very simple notification app that runs in a loop. I've put very little effort into this and will probably not develop it any further because it already does what I want and I don't mind just editing the code for each use case. It uses https://github.com/mikaelbr/node-notifier to do the notifications.
 
 ```JavaScript
+git clone https://github.com/bobonthenet/notifier.git
 npm install
 node notify.js
 ```
--- notify.js
+++ notify.js
@@ -4,6 +4,23 @@ const path = require("path");
 const looptime = 20 * 60 * 1000;
 const repeats = 3;
 const myMessage = "Hey! It is time to get up and walk around now.";
+
+notifier.notify({
+    title: "Initial notification!",
+    message: 'Further notifications will be sent after the looptime interval.',
+    sound: true,
+    contentImage: path.join(__dirname, "walk.png"),
+    icon: path.join(__dirname, "walk.png"),
+    wait: false
+  },
+  function (err, response, metadata) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('I\'m just doing my thing, you can minimize the console...')
+    }
+  });
+
 (function myLoop(i) {
   setTimeout(function () {
     notifier.notify({
